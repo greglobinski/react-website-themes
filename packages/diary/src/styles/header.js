fbd7d80f8b933a0caf89f10978827fb5ce7e7f4d
greglobinski <greglobinski@gmail.com>
2018-09-06 16:24:38 +0200

    fix(diary): add max-width to Header

--- packages/diary/src/styles/header.js
+++ packages/diary/src/styles/header.js
@@ -7,6 +7,8 @@ const mq = facepaint(breakpoints.map(bp => `@media (min-width: ${bp}px)`));
 const header = css`
   display: flex;
   flex-direction: column;
+  max-width: 600px;
+  margin: 0 auto;
 
   ${mq({
     padding: ['60px 0;', '60px 0 50px;'],
