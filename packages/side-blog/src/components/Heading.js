1fc0622db6db2198df698ec660011d0f1935ae42
greglobinski <greglobinski@gmail.com>
2018-09-18 21:57:57 +0200

    fix(side-blog): expand style of special Heading

--- packages/side-blog/src/components/Heading.js
+++ packages/side-blog/src/components/Heading.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { cx } from 'emotion';
 
 import style from '../styles/heading';
+import { getByTitle } from '../../../../node_modules/react-testing-library';
 
 const Heading = props => {
   const {
@@ -10,11 +11,13 @@ const Heading = props => {
     children,
     themeStyle = style,
     customStyle = '',
-    home = false,
+    special = false,
   } = props;
 
   return (
-    <header className={`${cx(themeStyle, customStyle)} ${home ? 'home' : ''}`}>
+    <header
+      className={`${cx(themeStyle, customStyle)} ${special ? 'special' : ''}`}
+    >
       {title ? <h1>{title}</h1> : children}
     </header>
   );
@@ -25,7 +28,7 @@ Heading.propTypes = {
   children: PropTypes.node,
   themeStyle: PropTypes.string,
   customStyle: PropTypes.string,
-  home: PropTypes.bool,
+  special: PropTypes.bool,
 };
 
 export default Heading;
--- packages/side-blog/src/styles/heading.js
+++ packages/side-blog/src/styles/heading.js
@@ -16,14 +16,34 @@ const heading = css`
       fontSize: ['2em', '2.3em', '2.6em'],
       padding: ['0 var(--spaceXL)', '0 var(--spaceXXL)'],
     })};
+
+    small {
+      font-size: 0.4em;
+      display: block;
+      font-weight: 300;
+      letter-spacing: 0.3em;
+      text-transform: uppercase;
+      margin: 0.5em 0;
+    }
+
+    span {
+      font-size: 0.5em;
+      display: block;
+      font-weight: 300;
+      letter-spacing: 0;
+      margin: 0.5em 0;
+    }
   }
 
-  &.home {
+  &.special {
     text-align: center;
+    margin-top: 2em;
 
     h1 {
+      letter-spacing: -0.05em;
+
       ${mq({
-        fontSize: ['2.8em', '3.2em', '3.6em'],
+        fontSize: ['2.5em', '2.8em', '3.2em'],
       })};
     }
   }
